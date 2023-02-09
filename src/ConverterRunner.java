17f9940fe4a708da494e235458912181461318cc
YunHuang123 <114007330+YunHuang123@users.noreply.github.com>
2023-02-09 18:28:00 -0500

    Finished

--- src/ConverterRunner.java
+++ src/ConverterRunner.java
@@ -1,81 +1,64 @@
 import java.util.Scanner;
-import java.util.Arrays;
 
 class ConverterRunner {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Number Converter!");
-        System.out.println("--------------------------------");
-        System.out.print("Enter the base of your number (2, 8 or 10): ");
+        int base = 0;
+        String num = "";
+        String thing = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+/";
 
         Scanner s = new Scanner(System.in);
-        String choice = s.nextLine();
-        while (!choice.equals("2") && !choice.equals("8") && !choice.equals("10"))
-        {
-            System.out.println("Sorry, this base is not available.");
-            System.out.print("Enter the base of your number (2, 8 or 10): ");
-            choice = s.nextLine();
-        }
-        int base = Integer.parseInt(choice);
+        System.out.println("Welcome to the Number Converter! No funny business");
+        System.out.println("--------------------------------");
+        System.out.print("Enter a base (1-64): ");
 
-        System.out.print("Enter your number: ");
-        String number = s.nextLine();
-        boolean hasLetter = false;
-        int n = 0;
-        for (int i = 0; i < number.length(); i ++)
-        {
-            if (Character.isLetter(number.charAt(i)))
-            {
-                hasLetter = true;
+        for (int i = 0; i < 1; i++) {
+            String choice = s.nextLine();
+            if (Integer.parseInt(choice) > 0 && Integer.parseInt(choice) < 65) {
+                base = Integer.parseInt(choice);
+            } else {
+                System.out.println("Not an available base");
+                System.out.print("Enter base (1-64): ");
+                i--;
             }
         }
 
-        if (hasLetter == false)
-        {
-            if (choice.equals("2"))
-            {
-                while (number.contains("2") || number.contains("3") || number.contains("4") || number.contains("5") || number.contains("6") || number.contains("7") || number.contains("8") || number.contains("9"))
-                {
-                    System.out.println("Sorry, this number contains incorrect digit(s) for this base.");
-                    System.out.print("Enter your number: ");
-                    number = s.nextLine();
+        System.out.print("Enter a positive number: ");
+        String number;
+        boolean temp = true;
+        while(temp){
+            temp = false;
+            number = s.nextLine();
+            for (int j = 0; j < number.length(); j++) {
+                if (thing.indexOf(number.charAt(j)) < 0) {
+                    temp = true;
+                    System.out.println("Invalid Number");
+                    System.out.print("Enter a number: ");
                 }
-            }
-            if (choice.equals("8"))
-            {
-                while (number.contains("8") || number.contains("9"))
-                {
-                    System.out.println("Sorry, this number contains incorrect digit(s) for this base.");
-                    System.out.print("Enter your number: ");
-                    number = s.nextLine();
+                if (thing.indexOf(number) >= base) {
+                    temp = true;
+                    System.out.println("Number does not compute with base input, Try again:");
                 }
+
             }
-            n = Integer.parseInt(number);
+            num = number;
         }
-        else
-        {
-            System.out.println("Invalid Input");
+        NumberConverter nc = new NumberConverter(num, base);
+        if (base != 2) {
+            System.out.println("Bi: " + nc.display(nc.convertEverything(2)));
         }
+        if (base != 8) {
+            System.out.println("Oct: " + nc.display(nc.convertEverything(8)));
+        }
+        if (base != 10) {
+            System.out.println("Deci: " + nc.display(nc.convertToDecimal()));
+        }
+        System.out.println("Choose another base to convert to: ");
+        int newBase = s.nextInt();
+        if (newBase > 64 || newBase < 1) {
+            System.out.println("Invalid base, please try again by restarting");
+            System.exit(0);
+        } else
+            System.out.println(newBase + ": " + nc.display(nc.convertEverything(newBase)));
 
-        s.close();
-
-        NumberConverter nc = new NumberConverter(n, base);
-        int[] digits = nc.getDigits();
-        if (base == 2)
-        {
-            System.out.println("Octal: " + nc.convertToOctal());
-            System.out.println("Decimal: " + nc.convertToDecimal());
-        }
-        else if (base == 8)
-        {
-            System.out.println("Binary: " + nc.convertToBinary());
-            System.out.println("Decimal: " + nc.convertToDecimal());
-        }
-        else if (base == 10)
-        {
-            System.out.println("Binary: " + nc.convertToBinary());
-            System.out.println("Octal: " + nc.convertToOctal());
-        }
-        System.out.println("\n\nDigit array: " + Arrays.toString(digits));
-        System.out.println("Number: " + nc.displayOriginalNumber());
     }
 }
--- src/NumberConverter.java
+++ src/NumberConverter.java
@@ -1,81 +1,93 @@
-public class NumberConverter
-{
-    int[] digits;
-    int base;
-    int num;
+public class NumberConverter {
+    private int[] digits;
+    private int base;
+    private String originalNum;
+    private final String forConversion = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+/";
+    //char[] numsAndLettersForConv = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '+', '/'};
 
-    public NumberConverter(int number, int base)
-    {
-        num = number;
-        String numberAsString = Integer.toString(number);
+    public NumberConverter(String number, int base) {
+        String numberAsString = number;
         digits = new int[numberAsString.length()];
-        for (int i = 0; i < numberAsString.length(); i++)
-        {
-            String single = numberAsString.substring(i,i+1);
-            int d = Integer.parseInt(single);
+        for (int i = 0; i < numberAsString.length(); i++) {
+            int d = forConversion.indexOf(String.valueOf(number.charAt(i)));
             digits[i] = d;
         }
         this.base = base;
     }
 
-    public String displayOriginalNumber()
-    {
+    public String display(int[] x) {
         String o = "";
-        for (int i = 0; i < digits.length; i++)
-        {
-            o = o + digits[i];
+        for (int i = 0; i < x.length; i++) {
+            o += forConversion.charAt(x[i]);
         }
-        o = o + "\n";
         return o;
     }
 
-
-
-
-    public String genBaseRule(int newBase)
-    {
-        String con = "";
-        int i = 0;
-        int b = 0;
-        while (i < digits.length) //converts to decimal
-        {
-            b += digits[(digits.length-1) - i]*Math.pow(base, i);
-            i++;
-        }
-        con = Integer.toString(b);
-
-
-        if (newBase != 10) //converts from decimal
-        {
-            con = "";
-            while (b > 0)
-            {
-                int c = b % newBase;
-                b /= newBase;
-                con = c + con;
-            }
-        }
-        return con;
-    }
-
-
-    public int[] getDigits()
-    {
+    public int[] getDigits() {
         return digits;
     }
 
-    public String convertToDecimal()
-    {
-        return genBaseRule(10);
+    public int[] convertToDecimal() {
+        int[] digitsTemp;
+        int temp = 0;
+        int pow = 0;
+        int x = 0;
+        for (int i = getDigits().length - 1; i >= 0; i--) {
+            temp += getDigits()[i] * Math.pow(base, x);
+            x++;
+        }
+        while (Math.pow(10, pow) <= temp) {
+            pow++;
+        }
+        digitsTemp = new int[pow];
+        pow--;
+        for (int i = 0; i < digitsTemp.length; i++) {
+            digitsTemp[i] = (int) (temp / Math.pow(10, pow));
+            temp -= digitsTemp[i] * (Math.pow(10, pow));
+            pow--;
+        }
+        return digitsTemp;
     }
 
-    public String convertToBinary()
-    {
-        return genBaseRule(2);
-    }
+    /**
+     * Failed experiment of changing it to return an integer. rip
+     *
+     * public int convertToDecimal() {
+     * int[] referenceToCharList = new int[getDigits().length - 1];
+     * int converted = 0;
+     * int x = 0;
+     * for(int i = getDigits().length - 1; i >= 0; i--){
+     * referenceToCharList[x] = forConversion.indexOf(getDigits()[i]);
+     * converted += referenceToCharList[x] * Math.pow(10, i);
+     * x++;
+     * }
+     * return converted;
+     * }
+     **/
 
-    public String convertToOctal()
-    {
-        return genBaseRule(8);
+
+    public int[] convertEverything(int convertBase) {
+        int pow = 0;
+        int[] x;
+        int num;
+
+        if (base != convertBase) {
+            if (base != 10) {
+                num = Integer.parseInt(display(convertToDecimal()));
+            }
+            else num = Integer.parseInt(display(digits));
+            while (Math.pow(convertBase, pow) <= num) {
+                pow++;
+            }
+            x = new int[pow];
+            pow--;
+            for (int i = 0; i < x.length; i++) {
+                x[i] = num / (int) (Math.pow(convertBase,pow));
+                num %= (int) (Math.pow(convertBase,pow));
+                pow--;
+            }
+            return x;
+        }
+        return null;
     }
 }
